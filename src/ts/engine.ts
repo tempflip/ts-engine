10e0705d173a8053ac947de6ae7b0179003554c1
Peter Tempfli <tempflip@gmail.com>
2018-06-19 10:25:11 +0200

    w

--- src/ts/engine.ts
+++ src/ts/engine.ts
@@ -3,10 +3,12 @@ const G : number = 3;
 abstract class Obj {
     x : number;
     y : number;
+    w : number = 19;
+    h : number = 19;
     vx : number = 0;
     vy : number = 0;
     timeStamp : number;
-
+    world : World;
 
     constructor(x : number, y : number) {
         this.x = x;
@@ -15,17 +17,21 @@ abstract class Obj {
     }
 
     public updateTimestamp() : void {
-        this.timeStamp = new Date().getTime();
+    this.timeStamp = new Date().getTime();
     }
 
     public step() : void {
         let t = new Date().getTime() - this.timeStamp;
         this.stepBehavior();
         this.updateTimestamp();
-        console.log('vx', this.vx);
+        // console.log('vx', this.vx);
     }
 
     public stepBehavior() : void {
+    }
+
+    public subscribeToWorld(world : World) : void {
+        this.world = world;
     }
 
 }
@@ -54,12 +60,24 @@ export class Player extends Obj {
     public stepBehavior() : void {
         this.vx *= 0.8;
         this.vy *= 0.8;
-        this.x += this.vx * 2;
-        this.y += G + this.vy * 2;
-
-        if (this.y > 500) this.y = 500;
+        let newX = this.x + this.vx * 2;
+        let newY = this.y + this.vy * 2 + G;
+        
+        // if (!this.world.isItCollision(newX, newY, this.w, this.h)) {
+        if (!this.world.isItCollision(this)) {
+            this.x = newX;
+            this.y = newY;
+        }
+        
+        // if (this.y > 500) this.y = 500;
 
     }
+}
+
+export class Brick extends Obj {
+    constructor(x : number, y : number) {
+        super(x,y);
+    }    
 }
 
 export class World {
@@ -74,11 +92,12 @@ export class World {
 
     public registerObj(objToRegister : Obj) : void {
         this.objList.push(objToRegister);
+        objToRegister.subscribeToWorld(this);
     }
 
     public draw() : void {
         for (let o of this.objList) {
-            this.ctx.fillRect(o.x, o.y, 15, 15);
+            this.ctx.fillRect(o.x, o.y, o.w, o.h);
         }
     }
 
@@ -92,6 +111,31 @@ export class World {
 
     public fire(win : Window, ms : number) : void {
         win.setInterval(() => {this.step();}, ms);
+    }
+
+    public isItCollision(obj : Obj) {
+        for (let o of this.objList) {
+
+            if (o == obj) continue;
+            let oxx = o.x + o.w;
+            let oyy = o.y + o.h;
+
+            let objxx = obj.x + obj.w
+            let objyy = obj.y + obj.h;
+
+
+
+            let xrule1 : Boolean = objxx > o.x && obj.x < oxx;
+            let xrule2 : Boolean= obj.x > o.x && obj.x < oxx;
+            let yrule1 : Boolean = obj.y < o.y && objyy >= o.y;
+            let yrule2 : Boolean= obj.y > o.y && obj.y < oyy;
+
+            if ((yrule1 || yrule2) && (xrule1)) {
+                console.log(yrule1, yrule2, o.y, oyy, obj.y, objyy);
+                return true;
+            }
+        }
+        return false;
     }
 }
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,20 @@
-import {Player, World} from "./engine";
+import {Player, World, Brick} from "./engine";
 
 let canvas = document.getElementById('canv');
 
-let p = new Player(100, 150);
+let p = new Player(300, 50);
 let w = new World(canvas);
 w.registerObj(p);
 
-document.addEventListener('keydown',(ev) => { p.onKeypress(ev) });
+for (var i = 10; i < 20; i++) {
+    let b = new Brick(i * 30, 300);
+    b.w = 25;
+    b.h = 30;
+    w.registerObj(b);   
+}
 
+
+
+document.addEventListener('keydown',(ev) => { p.onKeypress(ev) });
 w.fire(window, 10);
 
